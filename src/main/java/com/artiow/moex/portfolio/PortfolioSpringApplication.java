db09c911d9bd221d79a1b4c88df674f6d26164b9
artiow <namednev.artem@gmail.com>
2020-03-18 17:31:51 +0300

    Spring application initialization

---
+++ src/main/java/com/artiow/moex/portfolio/PortfolioSpringApplication.java
@@ -0,0 +1,12 @@
+package com.artiow.moex.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioSpringApplication.class, args);
+    }
+}
